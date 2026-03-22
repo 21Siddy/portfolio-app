3178c8f36a6dbe567736701e735de5f3b12bf972
21Siddy <siddharthhemnani1@gmail.com>
2026-03-22 14:05:41 +0100

    fixed staticfiles config with STORAGES dict

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -115,5 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = 'static/'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
